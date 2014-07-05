249881de79de7b3040f781b019759655b0e97e13
Srinivas Pradhan <srinivaspradhan64@gmail.com>
2014-07-05 13:59:45 -0400

    Cleanups start from Day 1

--- cookbooks/my_cookbook/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'my_cookbook'
-maintainer       'spradh02'
-maintainer_email 'YOUR_EMAIL'
-license          'Apache 2.0'
-description      'Installs/Configures my_cookbook'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
